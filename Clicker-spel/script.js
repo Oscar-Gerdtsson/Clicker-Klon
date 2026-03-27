49aba1964860797374f6fa52304e3a6f7eebca5e
Oscar-Gerdtsson <oscar.gerdtsson@elev.ga.lbs.se>
2026-03-27 10:31:58 +0100

    stuff

--- Clicker-spel/script.js
+++ Clicker-spel/script.js
@@ -1,11 +1,14 @@
-//  Variables
-let score = 0;
-function getScore(){return Math.floor(score)};
-function getClicksPerSecond(){return Math.floor(10*getClickPower()*autoclicker.amount/0.9**autoclickIntervalUpgrade.amount)/10}
-function getClickPower(){return 1+clickPowerUpgrade.amount};
-function getAutoclickInterval(){return 1000*0.9**autoclickIntervalUpgrade.amount};
-function getAutoclickSpeed(){return Math.round(10/0.9**autoclickIntervalUpgrade.amount)/10};
+//  Variabler
 
+let score = 0;
+function getClicksPerSecond(){return getClickPower()*autoclicker.amount/0.9**autoclickIntervalUpgrade.amount}//  score/s
+function getClickPower(){return (1+clickPowerPlusUpgrade.amount)*getClickPowerEfficiency()};//  score/click
+function getClickPowerEfficiency(){return 1.2**clickPowerEfficiencyUpgrade.amount}//  multiplier på click power
+function getAutoclickInterval(){return 1000*0.9**autoclickIntervalUpgrade.amount};//  ms/autoclick
+function getAutoclickSpeed(){return 1/0.9**autoclickIntervalUpgrade.amount};//  autoclicks/s
+function setAutoclickerInflation(){autoclicker.inflation= 1+(0.2*0.9**autoclickerInflationUpgrade.amount)}//  ändrar prisökningen på autoclickers
+
+// variabler för vissa html element
 const scoreDisplay = document.getElementById("score").lastElementChild;
 const clicksPerSecondDisplay = document.getElementById("clicks-per-second").lastElementChild;
 const clickPowerDisplay = document.getElementById("points-per-click").lastElementChild;
@@ -14,27 +17,31 @@ const autoclickSpeedDisplay = document.getElementById("autoclicks-per-second").l
 const targetArea = document.getElementById("click-target");
 const messageArea = document.getElementById("message-area");
 
-//  Upgrades
+
+//  Upgraderingar
 class Upgrade{
-    constructor(type, id, startingPrice, priceIncrease, amount){
+    constructor(type, id, startingPrice, Inflation, amount){
         this.type = type;
         this.id = id;
         this.startingPrice = startingPrice;
-        this.priceIncrease = priceIncrease;
+        this.inflation = Inflation;
         this.amount = amount;
     }
-    get price(){return Math.floor(this.startingPrice*this.priceIncrease**this.amount);}
+    get price(){return Math.floor(this.startingPrice*this.inflation**this.amount);}
 }
 
-const clickPowerUpgrade = new Upgrade("click","clickPowerUpgrade", 100,1.8,0);
+const clickPowerPlusUpgrade = new Upgrade("click", "clickPowerPlusUpgrade", 100, 1.8, 0)
+const clickPowerEfficiencyUpgrade = new Upgrade("click", "clickPowerEfficiencyUpgrade", 1000, 3, 0)
+const autoclickerInflationUpgrade = new Upgrade("autoclicker", "autoclickerInflationUpgrade", 10000, 10, 0)
 const autoclicker = new Upgrade("autoclicker","autoclicker", 10, 1.2, 0)
 const autoclickIntervalUpgrade = new Upgrade("autoclicker","autoclickIntervalUpgrade", 100, 2.5,0)
 
-const upgradesArr =[clickPowerUpgrade, autoclicker, autoclickIntervalUpgrade];
+
+const upgradesArr =[clickPowerPlusUpgrade, clickPowerEfficiencyUpgrade, autoclicker, autoclickIntervalUpgrade, autoclickerInflationUpgrade];
 
 
 
-//  Get localStorage
+//  hämtar localstorage
 if (localStorage.getItem("score")!= null) {
     score = parseInt(localStorage.getItem("score"));
 }
@@ -53,35 +60,40 @@ targetArea.addEventListener("click", click);
 document.getElementById(upgradesArr[0].id).addEventListener("click", function(){buyUpgrade(upgradesArr[0])});
 document.getElementById(upgradesArr[1].id).addEventListener("click", function(){buyUpgrade(upgradesArr[1])});
 document.getElementById(upgradesArr[2].id).addEventListener("click", function(){buyUpgrade(upgradesArr[2])});
+document.getElementById(upgradesArr[3].id).addEventListener("click", function(){buyUpgrade(upgradesArr[3])});
+document.getElementById(upgradesArr[4].id).addEventListener("click", function(){buyUpgrade(upgradesArr[4])});
 
 document.getElementById("reset-button").addEventListener("click", resetProgress);
 document.getElementById("save-button").addEventListener("click", saveProgress);
 
 
-//  On Start
-setInterval(saveProgress, 10000);
+//  vid start
+setInterval(saveProgress, 120000);//autosave varannan minut
 updateDisplay();
 autoclick();
 
-//  Basic Functions
-function updateDisplay(){
-    scoreDisplay.innerHTML = getScore();
-    clicksPerSecondDisplay.innerHTML = getClicksPerSecond();
-    clickPowerDisplay.innerHTML = getClickPower();
+//  funktioner
+function updateDisplay(){//uppdaterar stats m.m.
+    scoreDisplay.innerHTML = shortenLength(score)
+    clicksPerSecondDisplay.innerHTML = shortenLength(getClicksPerSecond());
+    clickPowerDisplay.innerHTML = shortenLength(getClickPower());
     autoclickersDisplay.innerHTML = autoclicker.amount;
-    autoclickSpeedDisplay.innerHTML = getAutoclickSpeed() + "/s"
+    autoclickSpeedDisplay.innerHTML = shortenLength(getAutoclickSpeed()) + "/s"
+    document.getElementById("clickPowerPlusUpgrade").firstElementChild.innerHTML ="+"+shortenLength(getClickPowerEfficiency())+" Click Power"
+
     upgradesArr.forEach(i => {
         document.getElementById(i.id).getElementsByClassName("price")[0].innerHTML = "<b>Price: </b>"+i.price+" Clicks <b>Amount: </b>"+i.amount;
     });
 
+    setAutoclickerInflation();// gör detta här för jag behöver skriva om mycket för att göra det på ett mer effektivt sätt
 }
-function saveProgress(){
+function saveProgress(){//sparar progress i localstorage
     upgradesArr.forEach(i => {
         localStorage.setItem(i.id+"amount", i.amount)
     });
     localStorage.setItem("score", score);
 }
-function autoclick(){
+function autoclick(){//autoclickar baserat på intervallen, om den är för snabb så byter den till en annan funktion på en intervall
     if (getAutoclickInterval()>20) {
         score+=autoclicker.amount*getClickPower();
         updateDisplay();
@@ -91,7 +103,7 @@ function autoclick(){
         setInterval(autoUpdate, 20);
     }
 }
-function autoUpdate(){
+function autoUpdate(){//den här funktionen används istället för autoclick om den blir callad för ofta
     score+=autoclicker.amount*getClickPower()/getAutoclickInterval();
     updateDisplay();
 }
@@ -99,7 +111,7 @@ function click(){
     score +=getClickPower();
     updateDisplay();
 }
-function buyUpgrade(id){
+function buyUpgrade(id){//id är uppgraderingen man köper
     if (score>=id.price) {
         score-=id.price;
         id.amount++;
@@ -109,6 +121,9 @@ function buyUpgrade(id){
 function resetProgress(){
     localStorage.clear();
     location.reload();
+}
+function shortenLength(num){//gör tal med lång decimal expansion kortare så de får plats i stats
+    return Math.round(10*num)/10
 }
 
 
