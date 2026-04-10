cb86f3dd3d536b6935bd408a064a6438d5ae9c0f
Oscar-Gerdtsson <oscar.gerdtsson@elev.ga.lbs.se>
2026-04-10 13:54:25 +0200

    lade till något som motsvarar golden cookies

--- Clicker-spel/script.js
+++ Clicker-spel/script.js
@@ -1,12 +1,15 @@
 //  Variabler
 
 let score = 0;
-function getClicksPerSecond(){return getClickPower()*autoclicker.amount/0.9**autoclickIntervalUpgrade.amount}//  score/s
-function getClickPower(){return (1+clickPowerPlusUpgrade.amount)*getClickPowerEfficiency()};//  score/click
+function getClicksPerSecond(){return getClickPower()*1000*autoclicker.amount/getAutoclickInterval()}//  score/s
+function getClickPower(){return (1+clickPowerPlusUpgrade.amount)*getClickPowerEfficiency()*95**Number(clickPowerBoost)};//  score/click
 function getClickPowerEfficiency(){return 1.2**clickPowerEfficiencyUpgrade.amount}//  multiplier på click power
-function getAutoclickInterval(){return 1000*0.9**autoclickIntervalUpgrade.amount};//  ms/autoclick
-function getAutoclickSpeed(){return 1/0.9**autoclickIntervalUpgrade.amount};//  autoclicks/s
+function getAutoclickInterval(){return 1000*0.9**autoclickIntervalUpgrade.amount*0.2**Number(speedBoost)};//  ms/autoclick
+function getAutoclickSpeed(){return 1000/getAutoclickInterval()};//  autoclicks/s
 function setAutoclickerInflation(){autoclicker.inflation= 1+(0.2*0.9**autoclickerInflationUpgrade.amount)}//  ändrar prisökningen på autoclickers
+//temporära effekter
+let speedBoost = false
+let clickPowerBoost = false
 
 // variabler för vissa html element
 const scoreDisplay = document.getElementById("score").lastElementChild;
@@ -16,6 +19,7 @@ const autoclickersDisplay = document.getElementById("autoclickers").lastElementC
 const autoclickSpeedDisplay = document.getElementById("autoclicks-per-second").lastElementChild;
 const targetArea = document.getElementById("click-target");
 const messageArea = document.getElementById("message-area");
+const popupWindow = document.getElementById("popup-window")
 
 
 //  Upgraderingar
@@ -71,6 +75,7 @@ document.getElementById("save-button").addEventListener("click", saveProgress);
 setInterval(saveProgress, 120000);//autosave varannan minut
 updateDisplay();
 autoclick();
+setTimeout(createPopup, 60000+Math.random()*120000)//spawnar en popup ett tag efter man startar spelet
 
 //  funktioner
 function updateDisplay(){//uppdaterar stats m.m.
@@ -92,6 +97,15 @@ function saveProgress(){//sparar progress i localstorage
     });
     localStorage.setItem("score", score);
 }
+function resetProgress(){
+    localStorage.clear();
+    location.reload();
+}
+
+function click(){
+    score +=getClickPower();
+    updateDisplay();
+}
 function autoclick(){//autoclickar baserat på intervallen, om den är för snabb så byter den till en annan funktion på en intervall
     if (getAutoclickInterval()>20) {
         score+=autoclicker.amount*getClickPower();
@@ -106,10 +120,7 @@ function autoUpdate(){//den här funktionen används istället för autoclick om
     score+=autoclicker.amount*getClickPower()/getAutoclickInterval();
     updateDisplay();
 }
-function click(){
-    score +=getClickPower();
-    updateDisplay();
-}
+
 function buyUpgrade(id){//id är uppgraderingen man köper
     if (score>=id.price) {
         score-=id.price;
@@ -117,19 +128,45 @@ function buyUpgrade(id){//id är uppgraderingen man köper
         updateDisplay();
     }
 }
-function resetProgress(){
-    localStorage.clear();
-    location.reload();
+
+function createPopup(){//slutar gömma popup fönstret och ger det en effekt
+    const popupText = document.getElementById("popup-info")
+    const popupButton = document.getElementById("popup-claim")
+    switch (Math.floor(Math.random()*3)+1) {//ändrar effekten på popuppen och ändrar infon i fönstret
+        case 1*(!speedBoost)://om man redan har effekten så får man inte den
+            popupText.innerHTML="Your autoclickers will be 5 times quicker for 3 minutes"
+            popupButton.setAttribute("onclick","reward1()")
+            break;
+        case 2*(!clickPowerBoost)://om man redan har effekten så får man inte den
+            popupText.innerHTML="Your click power will be 95 times higher for 10 seconds"
+            popupButton.setAttribute("onclick","reward2()")
+            break;
+        default:
+            popupText.innerHTML="You have been gifted 10 minutes of clicks"
+            popupButton.setAttribute("onclick","reward0()")
+            break;
+    }
+    popupWindow.style.display = "block"
 }
+function reward0(){//denna effekten ger en en mängd score
+    score+=getClicksPerSecond()*600
+    popupWindow.style.display = "none"//gömmer fönstret igen
+    setTimeout(createPopup, 10000+Math.random()*290000)//skapar en till popup efter ett tag
+}
+function reward1(){//denna effekten ger en snabbare autoclickers i ett tag
+    speedBoost=true
+    setTimeout(function(){speedBoost=false},180000)//tar bort effekten efter ett tag
+    popupWindow.style.display = "none"//gömmer fönstret igen
+    setTimeout(createPopup, 10000+Math.random()*290000)//skapar en till popup efter ett tag
+}
+function reward2(){//denna effekten ger en högre click power i ett tag
+    clickPowerBoost=true;
+    setTimeout(function(){clickPowerBoost=false},10000)//tar bort effekten efter ett tag
+    popupWindow.style.display = "none"//gömmer fönstret igen
+    setTimeout(createPopup, 10000+Math.random()*290000)//skapar en till popup efter ett tag
+}
+
 function shortenLength(num){//gör tal med lång decimal expansion kortare så de får plats i stats
     return Math.round(10*num)/10
 }
-
-
-
-
-
-
-//  testing
-
 
